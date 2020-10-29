302c3ae12090cd1fbf8e7915093ad18a8484b3aa
kreid333 <kai.j.reid@gmail.com>
2020-10-29 19:45:46 -0400

    added file layout and main jumbotron

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import "./Navbar.css";
+
+const Navbar = () => {
+  return (
+    <div className="jumbotron text-center" id="jumbo">
+      <div className="container">
+        <h2>Employee Directory</h2>
+        <p className="mt-3">
+          Click on the carrots to filter by heading or use the search box to
+          narrow your results
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
